569bb4cc7f266c61f76e165b57480a73d5dfba33
Siromom <swon962@gmail.com>
2020-04-20 10:28:38 +0900

    [Feat] #6 prettier 추가

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 
 export interface HelloProps {
-    compiler: string;
-    framework: string;
+  compiler: string;
+  framework: string;
 }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const Hello = (props: HelloProps) => (
+  <h1>
+    Hello from {props.compiler} and {props.framework}!
+  </h1>
+);
