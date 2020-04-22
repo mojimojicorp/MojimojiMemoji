fad9e623b197e64e908453119832e8409117d9fa
SeWonKim <swon962@gmail.com>
2020-04-22 16:32:50 +0900

    [Add] #2 webpack 설정 추가
    - 개발시 react 변경사항을 반영하려면 매번 build 해주거나 terminal을 동시에
    돌려야하는 불편함 때문에 webpack-dev-server에 대한 설정을 변경
    
    - 기존 index.html을 template으로 dist폴더에 새로운 html파일을 번들링하도록
    변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import App from './container/App';
+import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
