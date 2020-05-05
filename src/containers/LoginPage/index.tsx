10720ddd8fb73947242a9a63126d940d996bce06
soyoungjeong <hana46798@gmail.com>
2020-05-06 00:04:04 +0900

    [Feat] #5 로그인 페이지 UI 추가

---
+++ src/containers/LoginPage/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from './styles.scss';
+import logo from '../../images/body/logo_618x618.png';
+import naverLoginBtn from '../../images/body/naverLogin_btn.png';
+
+const LoginPage = () => {
+  return (
+    <div className={styles.background}>
+      <div className={styles.logo}>
+        <img src={logo} alt="" />
+      </div>
+      <div className={styles.title}>
+        <span>모지모지 메모지</span>
+      </div>
+      <div className={styles.login_btn}>
+        <img src={naverLoginBtn} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
