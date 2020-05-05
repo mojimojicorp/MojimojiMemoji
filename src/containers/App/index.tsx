7f606d5becb0069e52ab5d5c399d975aacd88b59
soyoungjeong <hana46798@gmail.com>
2020-05-06 02:59:14 +0900

    [Feat] #7 메모 수정 페이지 UI 추가

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -3,13 +3,14 @@ import styles from './styles.scss';
 import Header from '../../components/Header';
 import FolderList from '../../components/FolderList';
 import MemoList from '../../components/MemoList';
+import MemoEditPage from '../MemoEditPage';
 
 const App = () => {
-  return <div className={styles.App}>
-    <Header />
-    {/* <FolderList /> */}
-    <MemoList />
-         </div>;
+  return (
+    <div className={styles.App}>
+      <MemoEditPage />
+    </div>
+  );
 };
 
 export default App;
