7d79c4d896c3ec7cf3c9d989c0747263fe9d73db
Siromom <swon962@gmail.com>
2020-05-22 00:07:02 +0900

    [Chore] #30 UI 수정사항 반영
    1. Foler Setting, Memo Setting 페이지의 디자인 변경에 따른 수정사항 반영
    2. 버튼 삭제후 이미지형 아이콘으로 통일
    3. image sprite에 이미지 추가

--- app/src/components/FolderSetting/index.tsx
+++ app/src/components/FolderSetting/index.tsx
@@ -4,17 +4,24 @@ import styles from './styles.scss';
 const FolderSetting = () => {
   return (
     <div className={styles.folder_setting}>
-      <div className={styles.color_btn}>
-        <div className={styles.color} />
-        <div className={styles.picker} />
-      </div>
-      <div className={styles.move_btn}>
-        <div className={styles.move_icon} />
-        폴더 이동
-      </div>
-      <div className={styles.delete_btn}>
-        <div className={styles.delete_icon} />
-        폴더 삭제
+      <div className={styles.buttons}>
+        <div className={styles.color_btn}>
+          <div className={styles.color_picker}>
+            <div className={styles.color} />
+            <div className={styles.picker} />
+          </div>
+          색상 변경
+        </div>
+
+        <div className={styles.rename_btn}>
+          <div className={styles.rename_icon} />
+          이름 변경
+        </div>
+
+        <div className={styles.delete_btn}>
+          <div className={styles.delete_icon} />
+          폴더 삭제
+        </div>
       </div>
     </div>
   );
--- app/src/components/MemoSetting/index.tsx
+++ app/src/components/MemoSetting/index.tsx
@@ -4,13 +4,15 @@ import styles from './styles.scss';
 const MemoSetting = () => {
   return (
     <div className={styles.memo_setting}>
-      <div className={styles.move_btn}>
-        <div className={styles.move_icon} />
-        메모 이동
-      </div>
-      <div className={styles.delete_btn}>
-        <div className={styles.delete_icon} />
-        메모 삭제
+      <div className={styles.buttons}>
+        <div className={styles.move_btn}>
+          <div className={styles.move_icon} />
+          메모 이동
+        </div>
+        <div className={styles.delete_btn}>
+          <div className={styles.delete_icon} />
+          메모 삭제
+        </div>
       </div>
     </div>
   );
