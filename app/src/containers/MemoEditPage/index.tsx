20298f0c93116be9307217c79ebce776a7eaccce
SeWonKim <swon962@gmail.com>
2020-06-02 00:27:03 +0900

    [Feat] #37 contentEditable한 div 생성
    텍스트 입력을 위한 contentEditable한 div 생성

--- app/src/containers/MemoEditPage/index.tsx
+++ app/src/containers/MemoEditPage/index.tsx
@@ -4,15 +4,22 @@ import Header from '../../components/Header';
 import styles from './styles.scss';
 
 const MemoEditPage = () => {
+  function format(command: string) {
+    console.log(command);
+  }
+
   return (
     <div className={styles.edit_container}>
       <Header hasSaveBtn={true} hasBackBtn={true} />
-      
+
       <div className={styles.toolbar}>
-        <div className={styles.bold} />
-        <div className={styles.italic} />
-        <div className={styles.underline} />
-        <div className={styles.removeline} />
+        <div className={styles.bold} onClick={() => format('bold')} />
+        <div className={styles.italic} onClick={() => format('italic')} />
+        <div className={styles.underline} onClick={() => format('underline')} />
+        <div
+          className={styles.removeline}
+          onClick={() => format('strikeThrough')}
+        />
         <div className={styles.divider} />
         <div className={styles.align} />
         <div className={styles.divider} />
@@ -22,7 +29,7 @@ const MemoEditPage = () => {
         <div className={styles.logo} />
         <div className={styles.folder} />
       </div>
-      <div className={styles.edit_area} />
+      <div className={styles.edit_area} contentEditable="true" />
     </div>
   );
 };
